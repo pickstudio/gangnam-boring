7969480ce37c2542e15e03c801f72ea3285f653b
Jeontaeyun <jow1813@naver.com>
2023-03-01 14:52:03 +0900

    [FIX] Fix change file layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import ThemeConfig from "@/config/styles/theme";
-import "@/styles/reset.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+
+import ThemeConfig from "@/config/styles/theme";
+import "@/styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
