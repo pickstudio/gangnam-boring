3c736e0fdcae7356c37289ad24ae000fdc23430c
Jeontaeyun <jow1813@naver.com>
2023-03-01 16:37:29 +0900

    [FIX] Setting styled component with _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static async getInitialProps(context: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    try {
+      context.renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+      const initialProps = await Document.getInitialProps(context);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
