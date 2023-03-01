f8b2c274d9e040cb75fa86c4f9adf74c21951240
yeongjunekoh <rhdudwns0@naver.com>
2023-03-01 15:45:51 +0900

    [FEATURE] Generate Adress input bar

---
+++ pages/searchAdress/index.tsx
@@ -0,0 +1,24 @@
+import GBLayout from "@/components/base/GBLayout";
+import Head from "next/head";
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{"강남은 지루해"}</title>
+      </Head>
+      <GBLayout>
+        <Container></Container>
+      </GBLayout>
+    </React.Fragment>
+  );
+}
+
+const Container = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+`;
