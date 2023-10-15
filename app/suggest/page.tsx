4853ca15b64d9b3bdbda5615ad601e50b83f4f84
Jeontaeyun <jow1813@naver.com>
2023-10-15 15:12:05 +0900

    fix(*): bump up next version 13.5.4

--- app/suggest/page.tsx
+++ app/suggest/page.tsx
@@ -1,15 +1,22 @@
 import { Metadata } from 'next';
 
 export async function generateMetadata({ params, searchParams }: any): Promise<Metadata> {
+  console.log(searchParams);
   return {
     title: `이번엔 {${searchParams['place'] ?? ''}} ㅇㄸ?`,
     description: '장소가 마음에 들지 않는다면 클릭해서 다시 찾기',
     openGraph: {
       title: `이번엔 {${searchParams['place'] ?? ''}} ㅇㄸ?`,
       description: '장소가 마음에 들지 않는다면 클릭해서 다시 찾기',
-      url: `https://boring.dododot.net/badge?place=${searchParams['place']}&enPlace=${searchParams['enPlace']}`,
+      url: `https://boring.dododot.net/badge?place=${searchParams['place'] ?? ''}&enPlace=${
+        searchParams['enPlace'] ?? ''
+      }`,
       images: [
-        `https://boring.dododot.net/api/og/image?place=${searchParams['place']}&enPlace=${searchParams['enPlace']}`
+        {
+          url: `/api/og/image?place=${searchParams['place'] ?? ''}&enPlace=${searchParams['enPlace'] ?? ''}`,
+          width: 300,
+          height: 150
+        }
       ]
     }
   };
