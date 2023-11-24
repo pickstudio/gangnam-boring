45d2333964baab6cdec06506acf1e658f1b59938
yeongjunekoh <rhdudwns0@naver.com>
2023-11-24 16:38:56 +0900

    [FIX] Apply global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
-'use client';
+"use client";
 
-import { ThemeProvider } from 'styled-components';
-import RootStyleRegistry from '../lib/utils/server/RootStyleRegistry';
-import ThemeConfig from '../config/styles/theme';
+import { ThemeProvider } from "styled-components";
+import RootStyleRegistry from "../lib/utils/server/RootStyleRegistry";
+import ThemeConfig from "../config/styles/theme";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "../styles/reset.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
