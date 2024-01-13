da1bb0ba52a1bc935770aa15a3250781e0e4fbea
yeongjunekoh <rhdudwns0@naver.com>
2024-01-13 23:35:17 +0900

    [FEATURE] Generate Dummy data

--- api/dummy/response/MidPointDummyResponse/index.ts
+++ api/dummy/response/MidPointDummyResponse/index.ts
@@ -29,6 +29,177 @@ export const MidPointDummyResponse: MidPointAPIResponseType[] = [
           },
         ],
       },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57294132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57294132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44990403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44990403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44990403507674,
+          longitude: 126.57494132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44990403507674,
+            longitude: 126.57494132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
+      {
+        timeCost: 20,
+        numberOfTransfer: 2,
+        transportation: "BUS",
+        startPointLatLng: {
+          latitude: 33.44890403507674,
+          longitude: 126.57394132138821,
+        },
+        WayArrayLatLng: [
+          {
+            latitude: 33.45079660685329,
+            longitude: 126.57230632373583,
+          },
+          {
+            latitude: 33.44890403507674,
+            longitude: 126.57394132138821,
+          },
+        ],
+      },
     ],
   },
 ];
